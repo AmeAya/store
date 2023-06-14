7345974c78b8412b01ae3d0b66af86913bf842d1
AmeAya <ameameameaya@gmail.com>
2023-06-14 20:56:20 +0600

    check login available

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import psycopg2
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
 
 conn = psycopg2.connect(
     user='postgres',
-    password='5918',
+    password='5918', # change to your password
     host='127.0.0.1',
     port='5432',
-    database=''
+    database='store_db' # change to your db
 )
 cursor = conn.cursor()
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get('/')
 def homePage():
@@ -19,8 +21,14 @@ def homePage():
 
 @app.get('/reg')
 def registerPage():
-    return FileResponse('templates/home.html')
+    return FileResponse('templates/reg.html')
 
 @app.get('/check')
 def check(login):
-    pass
+    cursor.execute("SELECT login FROM users WHERE login='" + login + "';")
+    if cursor.fetchall():
+        return JSONResponse({'isAvailable': False})
+    else:
+        return JSONResponse({'isAvailable': True})
+# если логин занят return JSONResponse({'isAvailable': False})
+# иначе return JSONResponse({'isAvailable': True})
--- static/script.js
+++ static/script.js
@@ -1,1 +1,16 @@
-function check
+function checkLogin() {
+    var loginInput = document.getElementById('login');
+
+    var Request = new XMLHttpRequest();
+
+    Request.onreadystatechange = function() {
+        if (Request.readyState == 4) {
+            var response = JSON.parse(Request.responseText);
+            alert(response.isAvailable);
+        }
+    }
+
+    var url = 'http://127.0.0.1:8000/check?login=' + loginInput.value;
+    Request.open('get', url, true);
+    Request.send(null);
+}
